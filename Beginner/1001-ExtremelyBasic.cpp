4916044cf057df0bad2d5f8390c94db4fced3c07
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-01-25 11:38:20 +0200

    Create 1001-ExtremelyBasic.cpp

---
+++ Beginner/1001-ExtremelyBasic.cpp
@@ -0,0 +1,35 @@
+#include <cstdlib>
+#include<cstdio>
+
+int main() {
+int X,A,B;
+scanf("%d\n%d",&A,&B);
+X=A+B;
+printf("X = %d\n",X);
+    return 0;
+    
+}
+
+//#Extremely Basic
+//https://www.urionlinejudge.com.br/judge/en/problems/view/1001
+//
+//Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+//
+//Input
+//
+//The input file will contain 2 integer numbers.
+//
+//Output
+//
+//Print X according to the following example, with a blank space before and after the equal signal.
+//
+//Sample Inputs	Sample Outputs
+//10
+//9
+//X = 19
+//-10
+//4
+//X = -6
+//15
+//-7
+//X = 8
