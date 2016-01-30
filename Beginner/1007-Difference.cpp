b34d0bde1199ff1b51ac32d4a97caba926ab485c
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-01-30 19:32:23 +0200

    Create 1007-Difference.cpp

---
+++ Beginner/1007-Difference.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void) {
+int A,B,C,D,M;
+scanf("%d\n%d\n%d\n%d",&A,&B,&C,&D);
+M=(A*B)-(C*D);
+printf("DIFERENCA = %d\n",M);
+    return (0);
+}
