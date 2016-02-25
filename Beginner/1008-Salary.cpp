261e35bf12022ef30c860d34475d49b4b2138a8d
Ahmed Kamel Taha <ahmedengu@gmail.com>
2016-02-25 16:02:31 +0200

    Create 1008-Salary.cpp

---
+++ Beginner/1008-Salary.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+int n,h;
+float w,t;
+scanf("%d\n%d\n%f",&n,&h,&w);
+t=h*w;
+printf("NUMBER = %d\nSALARY = U$ %0.2f\n",n,t);
+	return (0);
+}
