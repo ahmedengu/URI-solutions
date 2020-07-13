e65fdf2a699c90e7c5001d5e300e8d66a0c215c6
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-07-13 11:25:17 +0200

    Create 1078 - Multiplication Table.cpp

---
+++ Beginner/1078 - Multiplication Table.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <stdio.h>
+#include <math.h>
+#include<cstdio>
+#include<string.h>
+using namespace std;
+
+int main(){
+	int x;
+	cin>>x;
+	for (int i = 1; i <= 10; ++i) {
+		cout <<i<<" x "<<x<<" = "<<i*x<<endl;
+
+	}
+	return 0;
+}
