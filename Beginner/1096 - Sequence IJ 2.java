bfece9d515f3407894deee62803053b1285eaa5e
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-07-18 14:09:22 +0200

    Create 1096 - Sequence IJ 2.java

---
+++ Beginner/1096 - Sequence IJ 2.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i+=2) {
+			for (int j = 7; j >= 5; j--) {
+				System.out.println("I="+i+" J="+j);				
+			}
+			
+		}
+	}
+
+}
