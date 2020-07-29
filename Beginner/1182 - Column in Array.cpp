759f39054275894005fc8685152bf676e4665eb4
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-07-29 17:43:10 +0200

    Create 1182 - Column in Array.cpp

---
+++ Beginner/1182 - Column in Array.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+
+int main() {
+	float x[12][12],c=0;
+	int l;
+	char s;
+	cin>>l;
+	cin>>s;
+	for (int i = 0; i < 12; ++i) {
+		for (int j = 0; j < 12; ++j) {
+			cin>>x[i][j];
+		}
+	}
+	for (int j = 0; j < 12; ++j) {
+		c+=x[j][l];
+	}
+	switch (s) {
+		case 'S':
+			printf("%0.1f\n",c);
+			break;
+		case 'M':
+			printf("%0.1f\n",c/12);
+			break;
+	}
+	return 0;
+}
