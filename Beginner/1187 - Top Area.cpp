ba5c936a24c678ea644b8988d4123894a725b3ab
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-07-30 20:41:32 +0200

    Create 1187 - Top Area.cpp

---
+++ Beginner/1187 - Top Area.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<cstdio>
+using namespace std;
+
+
+int main() {
+	double x[12][12],c=0;
+	int l=1,k=0;
+	char s;
+	cin>>s;
+	for (int i = 0; i < 12; ++i) {
+		for (int j = 0; j < 12; ++j) {
+			cin>>x[i][j];
+		}
+	}
+
+	for (int i = 0; i < 5; i++) {
+		for (int j = l; j <12-l; j++) {
+		c+=x[i][j];
+		k++;
+	}
+
+		l++;
+	}
+
+	switch (s) {
+		case 'S':
+			printf("%0.1f\n",c);
+			break;
+		case 'M':
+			printf("%0.1f\n",1.0*c/k);
+			break;
+	}
+	return 0;
+}
