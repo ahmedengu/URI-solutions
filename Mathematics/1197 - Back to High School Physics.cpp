816ee41334e97eb2ea0865463b5c21901664af50
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-07-31 21:09:07 +0200

    Create 1197 - Back to High School Physics.cpp

---
+++ Mathematics/1197 - Back to High School Physics.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cstdio>
+#include<cmath>
+#include<iomanip>
+#include <cstdlib>
+
+
+using namespace std;
+
+int main() {
+	int x,y;
+	while(scanf("%d",&x)==1){
+		scanf("%d",&y);
+		printf("%d\n",(x*y)*2);
+	}
+	return 0;
+}
