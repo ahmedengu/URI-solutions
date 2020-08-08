ca5ca345d77374467b7061a72d68705474c21e39
Ahmed Kamel Taha <ahmedengu@gmail.com>
2020-08-08 17:51:50 +0200

    Create 1478 - Square Matrix II.cpp

---
+++ Beginner/1478 - Square Matrix II.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include<cstdio>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int x[1000],o=-1,m[1000][1000],c=0,p=0,d=1;
+	do {
+		o++;
+		cin>>x[o];
+	} while (x[o]!=0);
+
+
+	for (int i = 0; i < o; ++i) {
+		c=0;
+
+			for (int r = 0; r < x[i]; ++r) {
+				c++;
+				m[r][0]=c;
+				p=c;
+				d=1;
+				for (int b = 1; b < x[i]; ++b) {
+					if(p>=2&&d==1)
+						p--;
+					else{
+						p++;
+						d=2;}
+					m[r][b]=p;
+			}
+			}
+
+
+		for (int r = 0; r < x[i]; ++r) {
+			printf("%3d",m[r][0]);
+			for (int b = 1; b < x[i]; ++b) {
+				printf(" %3d",m[r][b]);
+		}
+			printf("\n");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
